db2cc9678d6c6947bd48f4c16cdfb44f5e942a4e
Ryan Waits <ryan.waits@gmail.com>
2016-11-02 14:16:41 -0500

    add paperclip methods to formula

--- app/models/formula.rb
+++ app/models/formula.rb
@@ -1,3 +1,5 @@
 class Formula < ApplicationRecord
   belongs_to :user
+  has_attached_file :image, styles: { medium: "300x300>" }, dependent: true
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
