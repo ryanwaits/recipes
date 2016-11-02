c6b387568a18bf2bce374a77d4b5732e585f74c1
Ryan Waits <ryan.waits@gmail.com>
2016-11-02 13:10:02 -0500

    add routes for sessions and formulas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'formulas#index'
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+  resources :sessions, only: [:create]
+  resources :formulas
+
 end
