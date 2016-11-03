13ead7bb61ea6db82872c5ab244f580af53d282a
Ryan Waits <ryan.waits@gmail.com>
2016-11-03 16:04:37 -0500

    add associations and nested attributes method for ingredients and directions

--- app/models/formula.rb
+++ app/models/formula.rb
@@ -1,5 +1,13 @@
 class Formula < ApplicationRecord
   belongs_to :user
-  has_attached_file :image, styles: { medium: "300x300>" }, dependent: true
+  has_many :ingredients, dependent: :destroy
+  has_many :directions, dependent: :destroy
+
+  has_attached_file :image, styles: { medium: "300x300>" }, dependent: :destroy
+
+  accepts_nested_attributes_for :ingredients
+  accepts_nested_attributes_for :directions
+
+  validates_presence_of :title, :description
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
